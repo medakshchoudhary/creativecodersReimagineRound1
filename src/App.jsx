063633a3e55f920d790b586928f4889d836a0035
medakshchoudhary <ch.daksh01@gmail.com>
2024-06-29 16:01:40 +0530

    little progress made

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Navbar from './assets/components/Navbar';
+import Page1 from './assets/components/Page1';
 
 
 function App() {
   return (
-    <div className="bg-white w-full min-h-screen font-metropolis px-7 py-5">
+    <div className="bg-white w-full min-h-screen font-metropolis px-7 py-3">
       <Navbar />
-
+      <Page1 />
     </div>
   )
 }
