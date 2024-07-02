4fa8d4c790a193a6f4abbb2e848cad3d99aeb3ae
medakshchoudhary <ch.daksh01@gmail.com>
2024-07-02 12:09:06 +0530

    added animation to the collection cards

--- src/assets/components/Collections.jsx
+++ src/assets/components/Collections.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { motion } from 'framer-motion';
 
 function Collections() {
     const cards = [
@@ -6,22 +7,27 @@ function Collections() {
         { src: "/images/ig-stories.jpg", name: "For Instagram Stories" },
         { src: "/images/live-screenings.jpg", name: "Best of Live Screenings" },
         { src: "/images/newly-opened.jpeg", name: "Newly Opened Places" },
-      ];
-  return (
-    <div className="collections w-full min-h-screen">
-        <h1 className="m-0 pl-16 pt-16 text-[6vw]">Collections</h1>
-        <div className="cards-container flex gap-14 p-16">
-            {cards.map((card, index) => {
-                return(
-                    <div key={index} className="relative w-1/4 h-[24vw] card flex flex-col items-center">
-                        <img className="w-full h-full object-cover rounded-3xl" src={card.src} alt="" />
-                        <h1 className="absolute bottom-6 m-0 text-lg text-white">{card.name}</h1>
-                    </div>
-                );
-            })}
+    ];
+
+    return (
+        <div className="collections w-full min-h-screen">
+            <h1 className="m-0 pl-16 pt-16 text-[6vw]">Collections</h1>
+            <div className="cards-container flex gap-14 p-16">
+                {cards.map((card, index) => {
+                    return (
+                        <motion.div
+                            key={index}
+                            className="relative w-1/4 h-[24vw] card flex flex-col items-center cursor-pointer"
+                            whileHover={{ scale: 1.1, transition: { ease: "easeInOut" } }}
+                        >
+                            <img className="w-full h-full object-cover rounded-3xl" src={card.src} alt="" />
+                            <h1 className="absolute bottom-6 m-0 text-lg text-white">{card.name}</h1>
+                        </motion.div>
+                    );
+                })}
+            </div>
         </div>
-    </div>
-  )
+    );
 }
 
-export default Collections
+export default Collections;
