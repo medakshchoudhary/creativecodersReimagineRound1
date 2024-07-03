a049dfcd5521b25dcffe7dadff57f23ef6cafd48
Ankijit2 <ankijit714@gmail.com>
2024-07-03 18:28:39 +0530

    Done scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,24 @@ import LocomotiveScroll from 'locomotive-scroll';
 import Search from './assets/components/Search';
 import Collections from './assets/components/Collections';
 import Footer from './assets/components/Footer';
+import { AnimatePresence } from 'framer-motion';
 
 function App() {
   const locomotiveScroll = new LocomotiveScroll();
+
   return (
+   
     <div className="bg-white w-full min-h-screen font-metropolis">
+       <AnimatePresence>
       <Navbar />
       <LandingPage />
       <Marquee />
       <Search />
       <Collections />
       <Footer />
+      </AnimatePresence>
     </div>
+ 
   )
 }
 
