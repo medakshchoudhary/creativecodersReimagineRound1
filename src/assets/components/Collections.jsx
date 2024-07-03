201a7ea6b33179068bc8d7b960b357efe8605972
Ankijit2 <ankijit714@gmail.com>
2024-07-03 08:06:37 +0530

    Done

--- src/assets/components/Collections.jsx
+++ src/assets/components/Collections.jsx
@@ -12,12 +12,12 @@ function Collections() {
     return (
         <div className="collections w-full min-h-screen">
             <h1 className="m-0 lg:pl-16 md:pl-10 sm:pl-8 pl-4 lg:py-16 md:py-14 sm:py-12 py-10 lg:text-7xl md:text-5xl text-3xl ">Collections</h1>
-            <div className="cards-container grid md:grid-cols-4 grid-cols-2 xl:gap-20 lg:gap-18 md:gap-16 gap-14 px-10">
+            <div className="cards-container grid md:grid-cols-4 grid-cols-2 xl:gap-20 lg:gap-18 md:gap-10 gap-14  lg:px-10 px-8">
                 {cards.map((card, index) => {
                     return (
                         <motion.div
                             key={index}
-                            className="relative md:h-96 card flex flex-col items-center cursor-pointer"
+                            className="relative h-80 md:h-96 card flex flex-col items-center cursor-pointer"
                             whileHover={{ scale: 1.1, transition: { ease: "easeInOut" } }}
                         >
                             <img className="w-full h-full object-cover rounded-3xl" src={card.src} alt="" />
