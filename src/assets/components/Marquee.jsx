9b9cedff00ef01e1c49612417266e6ed2bb1d8e4
medakshchoudhary <ch.daksh01@gmail.com>
2024-07-03 17:43:57 +0530

    current commit

--- src/assets/components/Marquee.jsx
+++ src/assets/components/Marquee.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {motion} from 'framer-motion';
+import { motion } from 'framer-motion';
 
 function Marquee() {
   const foodItems = [
@@ -18,15 +18,20 @@ function Marquee() {
   return (
     <div data-scroll data-scroll-section data-scroll-speed="-0.001" className="marquee mt-2 p-9 bg-[#E23744] flex rounded-t-3xl">
       {foodItems.map((item, index) => (
-        <div key={index} className="pr-8">
-          <motion.div initial={{x:"0%"}} animate={{x:"-100%"}} transition={{repeat: Infinity ,ease:'linear',duration:5}} className="food-card px-4 pt-4 border-2 border-solid border-white rounded-3xl flex flex-col items-center justify-center">
-            <img className="w-[16vw] rounded-full" src={item.src} alt={item.name} />
-            <h1 className="text-white leading-none text-[3vw]">{item.name}</h1>
+        <div key={index} className="food-card-container pr-8">
+          <motion.div
+            initial={{ x: "0%" }}
+            animate={{ x: "-100%" }}
+            transition={{ repeat: Infinity, ease: 'linear', duration: 5 }}
+            className="food-card w-[42vw] h-[48vw] md:w-[20vw] md:h-[24vw] p-5 md:p-7 border-2 border-solid border-white rounded-3xl flex flex-col items-center justify-center"
+          >
+            <img className=" rounded-full object-cover" src={item.src} alt={item.name} />
+            <h1 className="text-white leading-none mt-3 md:mt-6 text-[5vw] md:text-[2.2vw]">{item.name}</h1>
           </motion.div>
         </div>
       ))}
     </div>
-  )
+  );
 }
 
-export default Marquee
+export default Marquee;
